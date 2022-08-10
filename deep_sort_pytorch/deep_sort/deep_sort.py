746a09bf6aefa50fe92d3a065b29f1460e535907
AmirHossein Vatanibaf <amirhosseinv842@gmail.com>
2022-08-10 13:41:23 +0430

    Add files

--- deep_sort_pytorch/deep_sort/deep_sort.py
+++ deep_sort_pytorch/deep_sort/deep_sort.py
@@ -52,6 +52,11 @@ class DeepSort(object):
             outputs = np.stack(outputs, axis=0)
         return outputs
 
+    """
+    TODO:
+        Convert bbox from xc_yc_w_h to xtl_ytl_w_h
+    Thanks JieChen91@github.com for reporting this bug!
+    """
     @staticmethod
     def _xywh_to_tlwh(bbox_xywh):
         if isinstance(bbox_xywh, np.ndarray):
@@ -71,6 +76,11 @@ class DeepSort(object):
         return x1, y1, x2, y2
 
     def _tlwh_to_xyxy(self, bbox_tlwh):
+        """
+        TODO:
+            Convert bbox from xtl_ytl_w_h to xc_yc_w_h
+        Thanks JieChen91@github.com for reporting this bug!
+        """
         x, y, w, h = bbox_tlwh
         x1 = max(int(x), 0)
         x2 = min(int(x+w), self.width - 1)
--- deep_sort_pytorch/deep_sort/sort/track.py
+++ deep_sort_pytorch/deep_sort/sort/track.py
@@ -1,3 +1,5 @@
+# vim: expandtab:ts=4:sw=4
+
 
 class TrackState:
     """
